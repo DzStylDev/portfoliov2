ab8537f9e39cbc21e09f20b2144583c51b9e5125
DzStylDev <amine.meddour@epitech.eu>
2024-10-23 22:40:59 +0200

    add logic of toggle , and show in menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -45,25 +45,41 @@ export default function Header({ scrollToSection }) {
           </button>
         </ul>
 
-        <FontAwesomeIcon icon={faBars} className='xl:hidden block text-5xl cursor-pointer size-6' onClick={() => setMenuVisible(!menuVisible)}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={!menuVisible ? faBars : faCircleXmark} color="#2d3e5a"  className='xl:hidden block text-5xl cursor-pointer size-6' onClick={() => setMenuVisible(!menuVisible)}></FontAwesomeIcon>
 
         <div
-          className={`absolute xl:hidden top-24 left-0 w-full [box-shadow:0_2px_4px_rgba(0,_0,_0,_0.5)] backdrop-blur-sm bg-[rgba(0,_0,_0,_0.5)]  flex flex-col items-center gap-6 font-semibold text-lg transform z-40 transition-transform ${menuVisible ? "opacity-100" : "opacity-0"}`}
+          className={`absolute xl:hidden top-24 left-0 w-full [box-shadow:0_2px_4px_rgba(0,_0,_0,_0.5)] backdrop-blur-sm bg-[rgba(0,_0,_0,_0.5)]  flex flex-col items-center gap-6 font-semibold text-lg transform z-40 transition-transform ${menuVisible ? "block" : "hidden"}`}
           style={{ transition: "transform 0.3s ease , opacity .3s ease" }}
         >
           <button className='p-4 list-none hover:bg-[#5e79a7] w-full rounded-md transition-all cursor-pointer text-center'>
             Accueil
           </button>
-          <button onClick={() => scrollToSection('about')} className='p-4 w-full list-none text-center hover:bg-[#5e79a7] rounded-md transition-all cursor-pointer'>
+          <button onClick={() => {
+              scrollToSection('about')
+              setMenuVisible(false)
+            }
+          } className='p-4 w-full list-none text-center hover:bg-[#5e79a7] rounded-md transition-all cursor-pointer'>
             A propos
           </button>
-          <button onClick={() => scrollToSection('competences')} className='p-4 text-center w-full list-none hover:bg-[#5e79a7] rounded-md transition-all cursor-pointer'>
+          <button onClick={() => {
+              scrollToSection('competences')
+              setMenuVisible(false)
+            }
+          } className='p-4 text-center w-full list-none hover:bg-[#5e79a7] rounded-md transition-all cursor-pointer'>
             Compétences
           </button>
-          <button onClick={() => scrollToSection('projects')} className='p-4 text-center w-full list-none hover:bg-[#5e79a7] rounded-md transition-all cursor-pointer'>
+          <button onClick={() => {
+              scrollToSection('projects')
+              setMenuVisible(false)
+            }
+          } className='p-4 text-center w-full list-none hover:bg-[#5e79a7] rounded-md transition-all cursor-pointer'>
             Projets
           </button>
-          <button onClick={() => scrollToSection('contact')} className='p-4 text-center list-none w-full hover:bg-[#5e79a7] rounded-md transition-all cursor-pointer'>
+          <button onClick={() => {
+              scrollToSection('contact')
+              setMenuVisible(false)
+            }
+          } className='p-4 text-center list-none w-full hover:bg-[#5e79a7] rounded-md transition-all cursor-pointer'>
             <span>Contact</span>
           </button>
         </div>
