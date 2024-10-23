ebec598af3dc2d61555b29999547284be6a5e175
DzStylDev <amine.meddour@epitech.eu>
2024-10-23 23:07:32 +0200

    add icon faCircleXMark

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { motion } from "framer-motion"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
 export default function Header({ scrollToSection }) {
 
   const [menuVisible, setMenuVisible] = useState(false)
