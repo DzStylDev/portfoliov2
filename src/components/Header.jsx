347a33454d32454b7f49b6eada799609a1531c3d
DzStylDev <amine.meddour@epitech.eu>
2024-10-24 00:40:49 +0200

    component Header : add class css for items and motion-framer for animated items in menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
-import { motion } from "framer-motion"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faCircleXmark } from '@fortawesome/free-solid-svg-icons'
+import { AnimatePresence, motion } from 'framer-motion'
+import { Squash as Hamburger } from "hamburger-react"
+
 export default function Header({ scrollToSection }) {
 
   const [menuVisible, setMenuVisible] = useState(false)
@@ -28,61 +28,116 @@ export default function Header({ scrollToSection }) {
 
 
         <ul className={`hidden xl:flex items-center gap-12 font-semibold text-sm`} style={{ transition: "transform 0.3s ease, opacity 0.3s ease" }} >
-          <button  className='p-3 hover:bg-[#5e79a7] rounded-md transition-all cursor-pointer'>
+          <button className='p-3 hover:bg-white hover:text-[#243147] rounded-md transition-all cursor-pointer'>
             Accueil
           </button>
-          <button onClick={() => scrollToSection('about')} className='p-3  cursor-pointer hover:bg-[#5e79a7] rounded-md transition-all'>
+          <button onClick={() => scrollToSection('about')} className='p-3  cursor-pointer hover:bg-white hover:text-[#243147] rounded-md transition-all'>
             A propos
           </button>
-          <button onClick={() => scrollToSection('competences')} className='p-3  cursor-pointer hover:bg-[#5e79a7] rounded-md transition-all'>
+          <button onClick={() => scrollToSection('competences')} className='p-3 cursor-pointer hover:bg-white hover:text-[#243147] rounded-md transition-all'>
             Compétences
           </button>
-          <button onClick={() => scrollToSection('projects')} className='p-3  cursor-pointer hover:bg-[#5e79a7] rounded-md transition-all'>
+          <button onClick={() => scrollToSection('projects')} className='p-3  cursor-pointer hover:bg-white hover:text-[#243147] rounded-md transition-all'>
             Projets
           </button>
-          <button onClick={() => scrollToSection('contact')} className='p-3  cursor-pointer hover:bg-[#5e79a7] rounded-md transition-all'>
+          <button onClick={() => scrollToSection('contact')} className='p-3  cursor-pointer hover:bg-white hover:text-[#243147] rounded-md transition-all'>
             <span>Contact</span>
           </button>
         </ul>
-
-        <FontAwesomeIcon icon={!menuVisible ? faBars : faCircleXmark} color="#2d3e5a"  className='xl:hidden block text-5xl cursor-pointer size-6' onClick={() => setMenuVisible(!menuVisible)}></FontAwesomeIcon>
-
-        <div
-          className={`absolute xl:hidden top-24 left-0 w-full [box-shadow:0_2px_4px_rgba(0,_0,_0,_0.5)] backdrop-blur-sm bg-[rgba(0,_0,_0,_0.5)]  flex flex-col items-center gap-6 font-semibold text-lg transform z-40 transition-transform ${menuVisible ? "block" : "hidden"}`}
-          style={{ transition: "transform 0.3s ease , opacity .3s ease" }}
-        >
-          <button className='p-4 list-none hover:bg-[#5e79a7] w-full rounded-md transition-all cursor-pointer text-center'>
-            Accueil
-          </button>
-          <button onClick={() => {
-              scrollToSection('about')
-              setMenuVisible(false)
-            }
-          } className='p-4 w-full list-none text-center hover:bg-[#5e79a7] rounded-md transition-all cursor-pointer'>
-            A propos
-          </button>
-          <button onClick={() => {
-              scrollToSection('competences')
-              setMenuVisible(false)
-            }
-          } className='p-4 text-center w-full list-none hover:bg-[#5e79a7] rounded-md transition-all cursor-pointer'>
-            Compétences
-          </button>
-          <button onClick={() => {
-              scrollToSection('projects')
-              setMenuVisible(false)
-            }
-          } className='p-4 text-center w-full list-none hover:bg-[#5e79a7] rounded-md transition-all cursor-pointer'>
-            Projets
-          </button>
-          <button onClick={() => {
-              scrollToSection('contact')
-              setMenuVisible(false)
-            }
-          } className='p-4 text-center list-none w-full hover:bg-[#5e79a7] rounded-md transition-all cursor-pointer'>
-            <span>Contact</span>
-          </button>
+        
+        <div className='xl:hidden flex text-5xl cursor-pointer'>
+          <Hamburger toggled={menuVisible} size={25} toggle={setMenuVisible} color='white' />
         </div>
+
+        <AnimatePresence>
+          {menuVisible && (
+            <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}
+              className={`absolute xl:hidden top-24 left-0 w-full [box-shadow:0_2px_4px_rgba(0,_0,_0,_0.5)] backdrop-blur-sm bg-[rgba(0,_0,_0,_0.5)] flex flex-col items-center gap-6 font-semibold text-lg transform z-40 transition-transform`}
+              style={{ transition: "transform 0.3s ease , opacity .3s ease" }}
+            >
+              <motion.button
+               initial={{ scale: 0, opacity: 0 }}
+               animate={{ scale: 1, opacity: 1 }}
+               transition={{
+                 type: "spring",
+                 stiffness: 260,
+                 damping: 20,
+                 delay: 0.1 + 0 / 10,
+               }} 
+                onClick={() => setMenuVisible(!menuVisible)} className='p-4 w-full list-none hover:bg-white hover:text-[#243147] transition-all cursor-pointer text-center [box-shadow:0_2px_4px_rgba(0,_0,_0,_0.5)] bg-[rgba(38,49,80,0.5)]'>
+                Accueil
+              </motion.button>
+              <motion.button
+               initial={{ scale: 0, opacity: 0 }}
+               animate={{ scale: 1, opacity: 1 }}
+               transition={{
+                 type: "spring",
+                 stiffness: 260,
+                 damping: 20,
+                 delay: 0.1 + 1 / 10,
+               }}  
+                onClick={() => {
+                scrollToSection('about')
+                setMenuVisible(!menuVisible)
+              }
+              } className='p-4 list-none text-center w-full hover:bg-white hover:text-[#243147] transition-all cursor-pointer [box-shadow:0_2px_4px_rgba(0,_0,_0,_0.5)] bg-[rgba(38,49,80,0.5)]'>
+                A propos
+              </motion.button>
+              <motion.button
+                initial={{ scale: 0, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 260,
+                  damping: 20,
+                  delay: 0.1 + 2 / 10,
+                }}  
+                onClick={() => {
+                  scrollToSection('competences')
+                  setMenuVisible(!menuVisible)
+              }
+              } className='p-4 text-center list-none w-full hover:bg-white hover:text-[#243147] transition-all cursor-pointer [box-shadow:0_2px_4px_rgba(0,_0,_0,_0.5)] bg-[rgba(38,49,80,0.5)]'>
+                Compétences
+              </motion.button>
+              <motion.button
+                initial={{ scale: 0, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 260,
+                  damping: 20,
+                  delay: 0.1 + 3 / 10,
+                }}  
+                onClick={() => {
+                  scrollToSection('projects')
+                  setMenuVisible(!menuVisible)
+              }
+              } className='p-4 text-center w-full list-none hover:bg-white hover:text-[#243147] rounded-md transition-all cursor-pointer [box-shadow:0_2px_4px_rgba(0,_0,_0,_0.5)] bg-[rgba(38,49,80,0.5)]'>
+                Projets
+              </motion.button>
+              <motion.button
+                initial={{ scale: 0, opacity: 0 }}
+                animate={{ scale: 1, opacity: 1 }}
+                transition={{
+                  type: "spring",
+                  stiffness: 260,
+                  damping: 20,
+                  delay: 0.1 + 4 / 10,
+                }}  
+                onClick={() => {
+                  scrollToSection('contact')
+                  setMenuVisible(!menuVisible)
+              }
+              } className='p-4 text-center list-none w-full hover:bg-white hover:text-[#243147] transition-all cursor-pointer [box-shadow:0_2px_4px_rgba(0,_0,_0,_0.5)] bg-[rgba(38,49,80,0.5)]'>
+                <span>Contact</span>
+              </motion.button>
+            </motion.div>
+          )}
+        </AnimatePresence >
 
 
       </motion.header>
